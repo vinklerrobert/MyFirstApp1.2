d26dc4df23fbb91348eaba3c16e993dd40fcf22e
ROBERT VINKLER <robert.vinkler@stud.ubbcluj.ro>
2020-10-16 16:49:16 +0300

    Am adaugat MyClass 1.2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstApp1._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+              Console.WriteLine("Hello World!");
+       }
     }
 }
